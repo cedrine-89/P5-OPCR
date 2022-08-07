b223cbde79616a11cb9e0df210d58ffb3b133c21
Stéphanie R <korettex@gmail.com>
2022-08-07 13:36:39 +0200

    Refactoring home and create object Api

---
+++ js/modules/Api.js
@@ -0,0 +1,17 @@
+export default class Api {
+    urlProducts = "http://192.168.1.11:3000/api/products";
+    error = false;
+
+    getApi() {
+        return fetch(this.urlProducts)
+            // get response to json
+            .then(response => response.json())
+            // get object json
+            .then(response => {
+                this.data = response;
+            })
+            .catch(e => {
+                this.error = `Error: ${e}`;
+            });
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 import ViewProduct from "./modules/ViewProduct.js";
-const url = "http://192.168.1.11:3000/api/products";
+import Api from "./modules/Api.js";
 
-// Fetch request
-fetch(url)
-    // get response to json
-    .then(response => response.json())
-    // get object json
-    .then(response => {
-        // Iteration object in table
-        for (const element of response) {
-            new ViewProduct(element);
+let api = new Api();
+
+api.getApi()
+    .then(() => {
+        for (const product of api.data) {
+            new ViewProduct(product);
         }
     });
