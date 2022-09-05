4cf6108b4a69c85960e859f3c830ae9f54011e5d
Stéphanie R <korettex@gmail.com>
2022-09-06 01:32:14 +0200

    Add Submit command

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,5 @@
 import Cart from "./modules/Cart.js";
+import FormSubmitCommand from "./modules/FormSubmitCommand.js";
 
 new Cart();
+new FormSubmitCommand();
---
+++ js/modules/FormSubmitCommand.js
@@ -0,0 +1,48 @@
+export default class FormSubmitCommand {
+    urlOrder = "http://192.168.1.11:3000/api/products/order";
+    nameObjectStorage = 'Products';
+
+    constructor() {
+        this.form = document.querySelector('#submitCommand');
+
+        this.form.addEventListener('submit', async (e) => {
+            // Stop Propagation formulaire
+            e.preventDefault();
+
+            const cartLocalStorage = JSON.parse(localStorage.getItem(this.nameObjectStorage));
+            let idProductForOrder = [];
+
+            cartLocalStorage.forEach(product => {
+                const [id, color] = product.id.split('-');
+                idProductForOrder.push(id);
+
+            });
+
+            const bodyData = {
+                contact: {
+                    firstName: document.querySelector('#firstName').value,
+                    lastName: document.querySelector('#lastName').value,
+                    address: document.querySelector('#address').value,
+                    city: document.querySelector('#city').value,
+                    email: document.querySelector('#email').value
+                },
+                products: idProductForOrder
+            }
+
+            const optionsPost = {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(bodyData)
+            }
+
+
+            const getPostResponse = await fetch(this.urlOrder, optionsPost);
+
+            const response = await getPostResponse.json();
+            window.location.replace(`confirmation.html?id=${response.orderId}`);
+        });
+    }
+}
