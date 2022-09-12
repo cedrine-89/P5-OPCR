3aa18d1d2f2f44cd87116638f6f29bd1eed06152
Stéphanie R <korettex@gmail.com>
2022-09-12 12:39:11 +0200

    Correction no form if empty cart

--- js/modules/FormSubmitCommand.js
+++ js/modules/FormSubmitCommand.js
@@ -21,102 +21,104 @@ export default class FormSubmitCommand {
     constructor() {
         this.form = document.querySelector('#submitCommand');
 
-        this.form.addEventListener('submit', async (e) => {
-            // Stop Propagation formulaire
-            e.preventDefault();
+        if (this.form) {
+            this.form.addEventListener('submit', async (e) => {
+                // Stop Propagation formulaire
+                e.preventDefault();
 
-            const cartLocalStorage = JSON.parse(localStorage.getItem(this.nameObjectStorage));
-            // Create table of Product-ID for Order
-            let idProductForOrder = [];
+                const cartLocalStorage = JSON.parse(localStorage.getItem(this.nameObjectStorage));
+                // Create table of Product-ID for Order
+                let idProductForOrder = [];
 
-            // Push Product ID in idProductForOrder
-            cartLocalStorage.forEach(product => {
-                const [id, color] = product.id.split('-');
-                idProductForOrder.push(id);
+                // Push Product ID in idProductForOrder
+                cartLocalStorage.forEach(product => {
+                    const [id, color] = product.id.split('-');
+                    idProductForOrder.push(id);
 
+                });
+
+                // Get value input form
+                const firstName = document.querySelector('#firstName').value;
+                const lastName = document.querySelector('#lastName').value;
+                const address = document.querySelector('#address').value;
+                const city = document.querySelector('#city').value;
+                const email = document.querySelector('#email').value;
+
+                // Validator input conforme
+                if (this.regExpString.test(firstName)) {
+                    this.firstNameErrorMsg.innerText = 'Erreur pas de chiffre dans votre Prénom';
+                    this.regExpString = new RegExp(/\d+/g);
+                    this.error = true;
+                    return false;
+                } else {
+                    this.firstNameErrorMsg.innerText = '';
+                    this.error = false;
+                }
+
+                if (this.regExpString.test(lastName)) {
+                    this.lastNameErrorMsg.innerText = 'Erreur pas de chiffre dans votre Nom';
+                    this.regExpString = new RegExp(/\d+/g);
+                    this.error = true;
+                    return false;
+                } else {
+                    this.lastNameErrorMsg.innerText = '';
+                    this.error = false;
+                }
+
+                if (this.regExpAddress.test(address)) {
+                    this.addressErrorMsg.innerText = 'Erreur votre Adresse ne doit pas commencer par un espace';
+                    this.error = true;
+                    return false;
+                } else {
+                    this.addressErrorMsg.innerText = '';
+                    this.error = false;
+                }
+
+                if (city === '') {
+                    this.cityErrorMsg.innerText = 'Erreur veuillez entrer que des lettres pour votre Ville';
+                    this.error = true;
+                    return false;
+                } else {
+                    this.cityErrorMsg.innerText = '';
+                    this.error = false;
+                }
+
+                if (this.regExpEmail.test(email)) {
+                    this.emailErrorMsg.innerText = '';
+                    this.error = false;
+                } else {
+                    this.emailErrorMsg.innerText = 'Erreur l\'adresse email est invalide !';
+                    this.error = true;
+                    return false;
+                }
+
+                if (!this.error) {
+                    const bodyData = {
+                        contact: {
+                            firstName: firstName,
+                            lastName: lastName,
+                            address: address,
+                            city: city,
+                            email: email
+                        },
+                        products: idProductForOrder
+                    }
+
+                    const optionsPost = {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(bodyData)
+                    }
+
+                    const getPostResponse = await fetch(this.urlOrder, optionsPost);
+                    const response = await getPostResponse.json();
+                    new LocalStorageDelete();
+                    window.location.replace(`confirmation.html?id=${response.orderId}`);
+                }
             });
-
-            // Get value input form
-            const firstName = document.querySelector('#firstName').value;
-            const lastName = document.querySelector('#lastName').value;
-            const address = document.querySelector('#address').value;
-            const city = document.querySelector('#city').value;
-            const email = document.querySelector('#email').value;
-
-            // Validator input conforme
-            if (this.regExpString.test(firstName)) {
-                this.firstNameErrorMsg.innerText = 'Erreur pas de chiffre dans votre Prénom';
-                this.regExpString = new RegExp(/\d+/g);
-                this.error = true;
-                return false;
-            } else {
-                this.firstNameErrorMsg.innerText = '';
-                this.error = false;
-            }
-
-            if (this.regExpString.test(lastName)) {
-                this.lastNameErrorMsg.innerText = 'Erreur pas de chiffre dans votre Nom';
-                this.regExpString = new RegExp(/\d+/g);
-                this.error = true;
-                return false;
-            } else {
-                this.lastNameErrorMsg.innerText = '';
-                this.error = false;
-            }
-
-            if (this.regExpAddress.test(address)) {
-                this.addressErrorMsg.innerText = 'Erreur votre Adresse ne doit pas commencer par un espace';
-                this.error = true;
-                return false;
-            } else {
-                this.addressErrorMsg.innerText = '';
-                this.error = false;
-            }
-
-            if (city === '') {
-                this.cityErrorMsg.innerText = 'Erreur veuillez entrer que des lettres pour votre Ville';
-                this.error = true;
-                return false;
-            } else {
-                this.cityErrorMsg.innerText = '';
-                this.error = false;
-            }
-
-            if (this.regExpEmail.test(email)) {
-                this.emailErrorMsg.innerText = '';
-                this.error = false;
-            } else {
-                this.emailErrorMsg.innerText = 'Erreur l\'adresse email est invalide !';
-                this.error = true;
-                return false;
-            }
-
-            if (!this.error) {
-                const bodyData = {
-                    contact: {
-                        firstName: firstName,
-                        lastName: lastName,
-                        address: address,
-                        city: city,
-                        email: email
-                    },
-                    products: idProductForOrder
-                }
-
-                const optionsPost = {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(bodyData)
-                }
-
-                const getPostResponse = await fetch(this.urlOrder, optionsPost);
-                const response = await getPostResponse.json();
-                new LocalStorageDelete();
-                window.location.replace(`confirmation.html?id=${response.orderId}`);
-            }
-        });
+        }
     }
 }
